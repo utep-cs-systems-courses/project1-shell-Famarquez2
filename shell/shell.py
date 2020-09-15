570f3a69010098afbd3d97e95f44fe50b6da0edc
Fernando Marquez <Ferniemarquez13@gmail.com>
2020-09-15 00:21:12 -0600

    Added Comment, need to add in all the python code and fix piping.

---
+++ shell/shell.py
@@ -0,0 +1,15 @@
+# Fernando Marquez
+
+# The following Python file is for the shell commands.
+# The result of this is to create a shell similar to terminal. The bash commands shall include:
+#       $ - To print string
+#       $ ls | sort -r) - For piping and sorting result
+#       $ $ find/etc -print & - For running background task(Having a task run on the background
+#       $ ls > /tmp/files.txt) - redirection of input and output
+#       $PATH - Finding the directory, folder or file path
+#       cd - change directory
+#       exit - end bash shell
+
+# The following program was taken from last semester (Spring 2020) when I attempted to take
+# Theory of OperatingSystems with Dr. Roach. I have for the most part converted my code from
+# C to python, however I've run into an issue with piping.
